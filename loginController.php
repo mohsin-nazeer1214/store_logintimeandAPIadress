3d4b176e1c79c17b56681675cb4384cc1a6b6335
mohsin-nazeer1214 <nazeermohsin187@gmail.com>
2023-02-20 16:40:25 +0500

    when ever userlogin user_id ip adress and also date will be inserted into the database

---
+++ loginController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        // Authenticate the user
+        if ($this->attemptLogin($request)) {
+            // Insert login history
+            DB::table('login_history')->insert([
+                'user_id' => Auth::id(),
+                'login_time' => now(),
+                'login_ip' => $request->ip(),
+            ]);
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // Handle failed login attempts
+        return $this->sendFailedLoginResponse($request);
+    }
+}
